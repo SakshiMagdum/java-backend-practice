3644ecac261e3b43141507798646b420f4e90455
Sakshi Magdum <sakshimagdum182004@gmail.com>
2025-12-30 22:05:12 +0530

    Add intial main class

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+        // Single line comment
+        /*This
+        is
+        a
+        multi-line
+        comment
+         */
+        System.out.print("Hello World!\n");
+        System.out.println("How are you doing!");
+        System.out.println("Lets learn java!");
+    }
+}
